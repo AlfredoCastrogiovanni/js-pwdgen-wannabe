f77829e11fe105e20a473027234019dc8fdec7ed
AlfredoCastrogiovanni <castro.alfry@gmail.com>
2023-11-09 13:08:04 +0100

    Esercizio completato

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+let nome = prompt("Inserisci il tuo nome: ");
+let cognome = prompt("Inserisci il tuo cognome: ");
+let colore = prompt("Inserisci il tuo colore preferito: ");
+
+document.getElementById("output").innerHTML = "La tua password e': " + nome + cognome + colore + "21";
+
